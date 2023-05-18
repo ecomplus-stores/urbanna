8c24a4b452523d28894ade91bf25dfaf0f5804d9
matheusgnreis <matheus@e-com.club>
2023-05-18 15:18:18 -0300

    chore(upgrade): step one from contract

---
+++ storefront.webpack.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
+      './js/ProductVariations.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductVariations.js')
+    }
+  }
+})
