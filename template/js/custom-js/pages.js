07628d135070e1171e94659954034680e92f829c
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-06-12 11:55:22 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,4 +1,2 @@
 // Add your custom JavaScript for storefront pages here.
-if (window.storefront && window.storefront.context && window.storefront.context.resource === 'products') {
-    window.$ecomConfig.set('default_img_size', 'zoom')
-}
+window.$ecomConfig.set('default_img_size', 'zoom')
