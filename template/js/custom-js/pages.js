e5460db63ae60eb2cb538b7b2c99a54a5c47ed8b
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-06-12 11:29:09 -0300

    chore: set zoom on product page

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,4 @@
 // Add your custom JavaScript for storefront pages here.
+if (window.storefront && window.storefront.context && window.storefront.context.resource === 'products') {
+    window.$ecomConfig.set('default_img_size', 'zoom')
+}
