edd127c3c20c8bcbdb2ff3a48304ed57ec729563
matheusgnreis <matheus@e-com.club>
2023-07-25 01:17:22 -0300

    chore: update store to 238

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -1,8 +1,11 @@
 const path = require('path')
+const dirSearchAlias = path.resolve(__dirname, 'template/js/lib/search-engine')
+const pathDslAlias = path.resolve(dirSearchAlias, 'dsl')
 
 module.exports = () => ({
   resolve: {
     alias: {
+      './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/components/TheProduct.js'),
       './js/ProductVariations.js': path.resolve(__dirname, 'template/js/custom-js/components/ProductVariations.js')
