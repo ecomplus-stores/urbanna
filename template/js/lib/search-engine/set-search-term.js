d9b88f139a7ca3e204f4fd68a298a18e849bb461
matheusgnreis <matheus@e-com.club>
2024-03-25 23:20:51 -0300

    chore: update search

--- template/js/lib/search-engine/set-search-term.js
+++ template/js/lib/search-engine/set-search-term.js
@@ -1,12 +1,50 @@
 import * as merge from 'lodash.merge'
+import query from '@ecomplus/search-engine/src/lib/dsl'
 
 export default (self, term) => {
   self.setSortOrder('news')
+  const arr = (term || '').split(' ')
+  /* const removeChar = (arr) => { 
+    if (arr.length === 1) {
+      return arr[0].replace(/(es)|s$/g, '')
+    }
+  } */
+  const fromTo = (arr) => {
+    const newArr = arr.map(word => {
+        const lower = word.toLowerCase()
+        switch (lower) {
+          case 'tenis':
+          case 'tênis':
+            return 'mocassim'
+          default:
+            return lower
+        }
+    })
+    /* if (arr.length === 2) {
+      switch(arr[0] + ' ' + arr[1]) {
+      }
+    }
+    if (arr.length === 3) {
+      switch(arr[0] + ' ' + arr[1] + ' ' + arr[2]) {
+      }
+    } */
+    return newArr.join(' ')
+  }
   // match name and/or keyword with term
   // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
+  if (term) {
+    const sort = query.sort.slice()
+    const relevanceSortIndex = sort.findIndex(s => s.ad_relevance)
+    sort.splice(relevanceSortIndex, 1)
+    self.dsl.sort = sort
+  }
+  console.log(self)
+  const modifiedTerm = fromTo(arr)
+  const finalTerm = modifiedTerm || term
   self.mergeFilter({
     multi_match: {
-      query: term,
+      query: finalTerm,
+      type: 'most_fields',
       fields: [
         'name',
         'keywords',
@@ -14,7 +52,7 @@ export default (self, term) => {
       ]
     }
   }, 'must')
-
+  
   merge(self.dsl, {
     // handle terms suggestion
     // 'did you mean?'
@@ -28,7 +66,6 @@ export default (self, term) => {
       }
     }
   })
-
   return self
 }
 
