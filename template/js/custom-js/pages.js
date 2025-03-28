84de2d252fe63efcd32372f4b740a546876de660
luizabrancaglion <92541373+luizabrancaglion@users.noreply.github.com>
2025-03-28 15:22:41 -0300

    redirecionamento para carrinho

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,7 +1,14 @@
-// Add your custom JavaScript for storefront pages here.
+// Primeiro trecho (busca de produtos)
 import EcomSearch from '@ecomplus/search-engine'
 window.$ecomConfig.set('default_img_size', 'zoom')
 
 const search = new EcomSearch()
 console.log(search)
 search.setSortOrder('news').fetch()
+
+// Segundo trecho (redirecionamento para carrinho)
+if (storefront && storefront.context && storefront.context.resource === 'products') {
+  ecomCart.on('addItem', (data) => { 
+    window.location = '/app/#/cart/'
+  })
+}
